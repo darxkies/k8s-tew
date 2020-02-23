a439a35e1e411e48ac14b69ca772d2075ef0f3b4
Mircea-Cristian Racasan <mircea.racasan@gmail.com>
2020-02-23 09:18:43 +0100

    Updated to Kubernetes v1.17.3

--- utils/constants.go
+++ utils/constants.go
@@ -2,7 +2,7 @@ package utils
 
 // Versions
 const VersionConfig = "2.3.0"
-const VersionK8s = "k8s.gcr.io/hyperkube:v1.16.7"
+const VersionK8s = "k8s.gcr.io/hyperkube:v1.17.3"
 const VersionEtcd = "quay.io/coreos/etcd:v3.4.3"
 const VersionContainerd = "1.3.3"
 const VersionRunc = "1.0.0-rc10"
