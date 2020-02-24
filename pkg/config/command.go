d0575f441548b349f2e542b6ac8b1dbb18005185
Mircea-Cristian Racasan <mircea.racasan@gmail.com>
2020-02-24 20:40:57 +0100

    Preparing to bypass kubectl during deployment

--- pkg/config/command.go
+++ pkg/config/command.go
@@ -2,7 +2,8 @@ package config
 
 type Command struct {
 	Name     string   `yaml:"name"`
-	Command  string   `yaml:"command"`
+	Command  string   `yaml:"command,omitempty"`
+	Manifest string   `yaml:"command,omitempty"`
 	Labels   Labels   `yaml:"labels,omitempty"`
 	Features Features `yaml:"features,omitempty"`
 	OS       OS       `yaml:"os,omitempty"`
@@ -13,4 +14,8 @@ type OS []string
 
 func NewCommand(name string, labels Labels, features Features, os OS, command string) *Command {
 	return &Command{Name: name, Labels: labels, Features: features, OS: os, Command: command}
+}
+
+func NewManifest(name string, labels Labels, features Features, os OS, manifest string) *Command {
+	return &Command{Name: name, Labels: labels, Features: features, OS: os, Manifest: manifest}
 }
